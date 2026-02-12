c837fadcbee6e12a6d0b83b6f380d2a428518d9b
federicotprogramador <federicotprogramador@gmail.com>
2026-02-12 15:32:49 -0300

    Footer y prueba

---
+++ landing-wgic26/src/app/test-iframe/page.tsx
@@ -0,0 +1,10 @@
+export default function TestIframePage() {
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <iframe
+        src="https://panel.helice.app/w/wgic26/214760/registration?preview=1"
+        style={{ width: "100%", height: "100%", border: "0" }}
+      />
+    </div>
+  );
+}
