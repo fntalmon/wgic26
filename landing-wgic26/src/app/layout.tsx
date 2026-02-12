bd60ef1bb2fd8267ee7c240743a015a3d0133852
federicotprogramador <federicotprogramador@gmail.com>
2026-02-12 09:42:53 -0300

    Update layout.tsx

--- landing-wgic26/src/app/layout.tsx
+++ landing-wgic26/src/app/layout.tsx
@@ -11,6 +11,18 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-38ME4KFC4N"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-38ME4KFC4N');
+          `,
+        }} />
+      </head>
       <body className="bg-monstera text-white font-light" style={{ fontFamily: 'Century Gothic, sans-serif' }}>
         <Navigation />
         <main className="flex flex-col gap-10 mt-28 lg:mt-40 mb-24 min-h-screen">
